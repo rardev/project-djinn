98e5bcb402940fbb1891c961b662d4315638e35b
Anthony-Roncone <andoro524@gmail.com>
2022-03-28 16:33:28 -0700

    updated login and signup pages

--- lib/pages/logIn.dart
+++ lib/pages/logIn.dart
@@ -17,7 +17,61 @@ class _logInState extends State<logIn> {
         centerTitle: true,
         elevation: 0.0,
       ),
-      body: const Text('Log in page'),
+      body: Column(
+        children: const [
+          textInputBox(label: 'Username', isPassword: false, boxSize: [200.0, 5.0, 200.0, 5.0]),
+          textInputBox(label: 'Password', isPassword: false, boxSize: [200.0,5.0,200.0,5.0]),
+          MaterialButton(
+            onPressed: doNothing,
+            color: Colors.green,
+            child: Text('Submit', style: TextStyle(color: Colors.white),),
+          ),
+        ],
+      )
     );
   }
+}
+
+class textInputBox extends StatefulWidget
+{
+  final String label;
+  final bool isPassword;
+  final List boxSize;
+  //final BoxSize boxSize;
+  const textInputBox({Key? key, required this.label, required this.isPassword, required this.boxSize}) : super(key: key);
+
+  @override
+  State<textInputBox> createState() => _textInputBoxState();
+}
+
+class _textInputBoxState extends State<textInputBox> {
+  final textController = TextEditingController();
+  //final double left = 200.0;
+  //List box = ;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(widget.boxSize[0], widget.boxSize[1], widget.boxSize[2], widget.boxSize[3]),
+      child: TextField(
+        controller: textController,
+        obscureText: widget.isPassword,
+        decoration: InputDecoration(
+            hintText: widget.label,
+            border: const OutlineInputBorder(),
+            suffixIcon: IconButton(
+              onPressed: (){
+                textController.clear();
+              },
+              icon: const Icon(Icons.clear),
+            )
+        ),
+      ),
+    );
+  }
+}
+
+void doNothing()
+{
+
 }
--- lib/pages/signUp.dart
+++ lib/pages/signUp.dart
@@ -8,6 +8,7 @@ class signUp extends StatefulWidget {
 }
 
 class _signUpState extends State<signUp> {
+  final textController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +18,73 @@ class _signUpState extends State<signUp> {
         centerTitle: true,
         elevation: 0.0,
       ),
-      body: const Text('This is the sign up page'),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(195.0, 0.0, 195.0, 0.0),
+            child: Row(
+              children: const [
+                Expanded(child: textInputBox(label: 'First Name', isPassword: false, boxSize: [5.0, 5.0, 5.0, 5.0])),
+                Expanded(child: textInputBox(label: 'Last Name', isPassword: false, boxSize: [5.0, 5.0, 5.0, 5.0])),
+              ],
+            ),
+          ),
+          const textInputBox(label: 'Username', isPassword: false, boxSize: [200.0,5.0,200.0,5.0]),
+          const textInputBox(label: 'Email', isPassword: false, boxSize: [200.0,5.0,200.0,5.0]),
+          const textInputBox(label: 'Password', isPassword: true, boxSize: [200.0,5.0,200.0,5.0]),
+          const textInputBox(label: 'Confirm Password', isPassword: true, boxSize: [200.0,5.0,200.0,5.0]),
+          const textInputBox(label: 'Phone Number', isPassword: false, boxSize: [200.0,5.0,200.0,5.0]),
+          const MaterialButton(
+              onPressed: doNothing,
+              color: Colors.green,
+              child: Text('Submit', style: TextStyle(color: Colors.white),),
+          ),
+        ],
+      ),
     );
   }
+}
+
+class textInputBox extends StatefulWidget
+{
+  final String label;
+  final bool isPassword;
+  final List boxSize;
+  //final BoxSize boxSize;
+  const textInputBox({Key? key, required this.label, required this.isPassword, required this.boxSize}) : super(key: key);
+
+  @override
+  State<textInputBox> createState() => _textInputBoxState();
+}
+
+class _textInputBoxState extends State<textInputBox> {
+  final textController = TextEditingController();
+  //final double left = 200.0;
+  //List box = ;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(widget.boxSize[0], widget.boxSize[1], widget.boxSize[2], widget.boxSize[3]),
+      child: TextField(
+        controller: textController,
+        obscureText: widget.isPassword,
+        decoration: InputDecoration(
+            hintText: widget.label,
+            border: const OutlineInputBorder(),
+            suffixIcon: IconButton(
+              onPressed: (){
+                textController.clear();
+              },
+              icon: const Icon(Icons.clear),
+            )
+        ),
+      ),
+    );
+  }
+}
+
+void doNothing()
+{
+
 }
